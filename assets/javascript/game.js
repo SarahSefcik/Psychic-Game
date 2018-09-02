cd47eb76c9ac54212cb8183508c1d7d8797c84dc
Sarah Catherine Sefcik <sefciksa@gmail.com>
2018-09-02 16:10:17 -0400

    finished logic and added reset function to remove simple resets within the logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,6 +5,8 @@ var choices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
 var wins = 0;
 var losses = 0;
 var guessCount = 9;
+var lettersGuessed = [];
+var lettersGuessedAsString = lettersGuessed.join(", ");
 
 // Create variables that hold references to the places in the HTML where we want to display things.
 var directionsText = document.getElementById("directions-text");
@@ -18,6 +20,12 @@ var yourGuessesSoFarText = document.getElementById("yourguessessofar-text");
 // This is the computer choice happening
 var computerGuess = choices[Math.floor(Math.random() * choices.length)];
 
+// This resets the game
+function resetGame() {
+  computerGuess = choices[Math.floor(Math.random() * choices.length)];
+  guessCount = 9;
+  lettersGuessed = [];
+}
 
 // This function is run whenever the user presses a key
 document.onkeyup = function (event) {
@@ -25,27 +33,41 @@ document.onkeyup = function (event) {
   // Determines which key was pressed
   var userGuess = event.key;
 
-  // Only run the following code if the user presses a letter
-  if (userGuess === computerGuess) {
+  // Pushes the user guess to the array
+  lettersGuessed.push(userGuess);
+
+  // Only run the following code if the user presses a letter. If match then:
+  if ((userGuess === computerGuess) && (guessCount > 0)) {
     wins++;
+
     // Reset
+    resetGame();
+    // guessCount = 9;
+    // lettersGuessed.length = 0;
   }
-  else if (guessCount === 0) {
-    losses++;
-    // Reset
-  }
-  else {
+
+  // Only run if not a match but the user still has guesses left
+  else if ((userGuess !== computerGuess) && (guessCount > 0)) {
     guessCount--;
-  
   }
-
-
   
+  // Only run if there are no more guesses left for the user
+  else {
+    losses++;
+
+    // Reset
+    resetGame();
+    // guessCount = 9;
+    // lettersGuessed.length = 0;
+  }
+  
+  
+
   // Displays references to the HTML
   winsText.textContent = "wins: " + wins;
   lossesText.textContent = "losses: " + losses;
   guessesLeftText.textContent = "Guesses left: " + guessCount;
-  yourGuessesSoFarText.textContent = "Your Guesses so far: " + userGuess;
+  yourGuessesSoFarText.textContent = "Your Guesses so far: " + lettersGuessed.join(', ');
   userChoiceText.textContent = "You chose: " + userGuess;
   computerChoiceText.textContent = "The computer chose: " + computerGuess;
 }
